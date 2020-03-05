4e8cb8920899c0fe117d52e7c879cc9f0b58a51d
Renato <re.cardozooo@hotmail.com>
2020-03-04 22:24:32 -0300

    Validating isbn number

--- src/main/java/com/example/tdd/isbn/ValidateIsbn.java
+++ src/main/java/com/example/tdd/isbn/ValidateIsbn.java
@@ -1,7 +1,24 @@
 package com.example.tdd.isbn;
 
+import java.util.Objects;
+
 public class ValidateIsbn {
-    public boolean checkISBNNumber(int isbnNumber) {
-        return true;
+    public boolean checkISBNNumber(String isbnNumber) {
+
+        int total = 0;
+        checkISBNSize(isbnNumber);
+
+        for (int i = 0; i < isbnNumber.length(); i++) {
+            total += isbnNumber.charAt(i) * (isbnNumber.length() - i);
+        }
+        return total % 11 == 0;
+    }
+
+    private void checkISBNSize(String isbnNumber) {
+        if (!Objects.equals(isbnNumber.length(),10)) {
+            throw new NumberFormatException("Número ISBN tem que ter dez caracteres");
+        }
+
+        Integer.parseInt(isbnNumber);
     }
 }
--- src/test/java/com/example/tdd/isbn/ValidateIsbnTest.java
+++ src/test/java/com/example/tdd/isbn/ValidateIsbnTest.java
@@ -19,14 +19,26 @@ class ValidateIsbnTest {
     @Test
     void checkValidISBN() {
         ValidateIsbn validateIsbn= new ValidateIsbn();
-        boolean result=validateIsbn.checkISBNNumber(140449116);
+        boolean result=validateIsbn.checkISBNNumber("0140449116");
         assertTrue(result);
+    }
+
+    @Test
+    void checkIsbnSize(){
+        ValidateIsbn validateIsbn= new ValidateIsbn();
+        assertThrows(NumberFormatException.class,() -> validateIsbn.checkISBNNumber("123456789"));
+    }
+
+    @Test
+    void checkIsbnIsNumber(){
+        ValidateIsbn validateIsbn= new ValidateIsbn();
+        assertThrows(NumberFormatException.class,() -> validateIsbn.checkISBNNumber("helloworld"));
     }
 
     @Test
     void checkInvalidValidISBN() {
         ValidateIsbn validateIsbn= new ValidateIsbn();
-        boolean result=validateIsbn.checkISBNNumber(140449116);
+        boolean result=validateIsbn.checkISBNNumber("0140447116");
         assertFalse(result);
     }
 }
